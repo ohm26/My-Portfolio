6aa41ada88c06c982650fc76625bb4f93b1965d3
ohm26 <hussienomar03@gmail.com>
2022-10-12 19:51:23 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.fa-bars').click(function () {
+        $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
+    });
+    $(window).on('scroll load', function () {
+        if ($(window).scrollTop() < 25) {
+            $('.header').hide();
+        }
+        else {
+            $('.header').show();
+        }
+       $('fa-bars').removeclass('fa-times');
+       $('navbar').removeclass('nav-toggle');
+    })
+});
